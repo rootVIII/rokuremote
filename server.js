e13e0bec52b500703b4e1c3ca9528fa293b675ec
rootVIII <colleyloyejames@gmail.com>
2021-07-27 23:03:15 -0400

    remove ENOENT check

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ const mimeTypes = {
     '.css': 'text/css',
     '.json': 'application/json',
     '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.wasm': 'application/wasm',
 };
 
 const page404 = `
@@ -78,13 +68,8 @@ const server = http.createServer((request, response) => {
     } else {
         fs.readFile(filePath, (error, content) => {
             if (error) {
-                if (error.code === 'ENOENT') {
-                    response.setHeader('Content-Type', 'text/html');
-                    response.end(page404);
-                } else {
-                    response.writeHead(500);
-                    response.end(`ERROR: ${error}`);
-                }
+                response.setHeader('Content-Type', 'text/html');
+                response.end(page404);
             } else {
                 response.writeHead(200, { 'Content-Type': contentType });
                 response.end(content, 'utf-8');
